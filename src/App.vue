b6a4cfc9458a76f6ead2c2dba54e340299f68c20
Jeremy Eissler <jeissler0@gmail.com>
2025-08-20 12:28:13 -0500

    goto solution for grouping w/ sample layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ItemList from '@/components/ItemList.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <h1>Page Header</h1>
+  </header>
+  <main class="flex">
+    <Suspense>
+      <ItemList />
+      <template #fallback>
+        <!-- loader component for reuse if using suspense -->
+        ...loading
+      </template>
+    </Suspense>
+  </main>
+  <footer>
+    <p>Page Footer</p>
+  </footer>
 </template>
-
-<style scoped></style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+interface User {
+  id: number
+  username: string
+  fullName: string
+}
+
+interface ResponseItem {
+  id: number
+  body: string
+  postId: number
+  likes: number
+  user: User
+}
+
+interface GroupedItem {
+  name: string
+  items: ResponseItem[]
+}
+
+const isLoading = ref(true)
+const items = ref<ResponseItem[]>([])
+
+const groupedItems = computed(() => {
+  // group items by user - goto
+  const grouped: GroupedItem[] = []
+  for (const item of items.value) {
+    const fullName = item.user.fullName
+    const group = grouped.find((i) => i.name === fullName)
+    if (group) group.items.push(item)
+    else grouped.push({ name: item.user.fullName, items: [item] })
+  }
+  return grouped
+})
+
+try {
+  // extract fetch wrapper to base util + composable
+  // check for correct headers/data types
+  // needs message handling
+  const res = await fetch('https://dummyjson.com/comments')
+
+  if (!res.ok) throw new Error(`Error: ${res.status}`)
+
+  const data = await res.json()
+
+  items.value = data.comments
+
+  isLoading.value = false
+} catch (err) {
+  console.error(err)
+}
+</script>
+
+<template>
+  <div>
+    <section v-for="user in groupedItems" :key="user.name" class="my-3 bg-indigo-100">
+      <header>{{ user.name }}</header>
+      <!--
+      <ul v-for="item in items" :key="item.id">
+        <li>{{ item }}</li>
+      </ul>
+      -->
+      <ul v-for="item in user.items" :key="item.id">
+        <li>{{ item.likes }} {{ item.body }}</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
